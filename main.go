f0109fd1b510780020b4cbf8ac72dfdf994121aa
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-08-16 13:41:35 +0100

    :sparkles: feat: add fizzbuzz count

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/myugen/fizzbuzz-kata/fizzbuzz"
+)
+
+func main() {
+	evaluator := fizzbuzz.NewEvaluator()
+	for counter := 1; counter <= 100; counter++ {
+		fmt.Println(evaluator.Print(counter))
+	}
 }
